cdcb3d21897b3d76821008d1eafa6a5ecf3a9e43
Yannick Schini <yannick.schini@qonto.com>
2022-01-22 17:32:04 +0100

    refacto(day3): generate a vector with all the bits in each position
    This is done to help the implementation of part 2, as I can now extract
    a function that parses this vector (different function for part 1 and 2)

--- day03/src/part1.rs
+++ day03/src/part1.rs
@@ -1,3 +1,4 @@
+use std::char;
 use std::env;
 use std::fs;
 use std::str;
@@ -8,21 +9,18 @@ fn main() {
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let lines: Vec<&str> = contents.lines().collect();
+
     let mut gamma_rate_string: String = "".to_string();
     let mut epsilon_rate_string: String = "".to_string();
 
     for bit in 0..lines[0].len() {
-        let mut bit_sum = 0usize;
+        let mut bit_vector: Vec<usize> = Vec::new();
         for line in &lines {
-            bit_sum = bit_sum + line.chars().nth(bit).unwrap().to_digit(10).unwrap() as usize
+            bit_vector.push(line.chars().nth(bit).unwrap().to_digit(10).unwrap() as usize);
         }
-        if bit_sum > lines.len()/2 {
-            gamma_rate_string.push('1');
-            epsilon_rate_string.push('0');
-        } else {
-            gamma_rate_string.push('0');
-            epsilon_rate_string.push('1');
-        }
+        let (most_common_bit, least_common_bit) = get_most_and_least_common_bits(bit_vector);
+        epsilon_rate_string.push(least_common_bit);
+        gamma_rate_string.push(most_common_bit);
     }
     println!("Gamma_rate_string: {}", gamma_rate_string);
     let gamma_rate = usize::from_str_radix(&gamma_rate_string, 2).unwrap();
@@ -30,4 +28,13 @@ fn main() {
     println!("Gamma rate in decimal: {}", gamma_rate);
     println!("Epsilon rate in decimal: {}", epsilon_rate);
     println!("Solution: {}", gamma_rate * epsilon_rate)
+}
+
+fn get_most_and_least_common_bits(arg: Vec<usize>) -> (char, char) {
+    let vec_sum: usize = arg.iter().sum();
+        if vec_sum > arg.len()/2 {
+            ('1', '0')
+        } else {
+            ('0', '1')
+        }
 }
