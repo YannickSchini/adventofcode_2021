6dd2618d7dd2158eb8ad549f4e3f8eefaa81f207
Yannick Schini <yannick.schini@qonto.com>
2022-02-01 09:54:04 +0100

    feat(day3): finish part2

--- day03/src/part2.rs
+++ day03/src/part2.rs
@@ -1,3 +1,4 @@
+use std::str;
 use std::usize;
 use std::env;
 use std::fs;
@@ -6,13 +7,29 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    
     let mut lines: Vec<&str> = contents.lines().collect();
     let mut index: usize = 0;
-    
     loop {
-        let filtered_lines = vector_function(lines, index);
+        let filtered_lines = vector_function(lines, index, "oxygen");
         if filtered_lines.len() <= 1 {
-            println!("Last line: {:?}", filtered_lines);
+            println!("Oxygen rate: {}", filtered_lines[0]);
+            let oxygen_rate = usize::from_str_radix(filtered_lines[0], 2).unwrap();
+            println!("{}", oxygen_rate);
+            break
+        }
+        index += 1;
+        lines = filtered_lines;
+    }
+
+    let mut lines: Vec<&str> = contents.lines().collect();
+    let mut index: usize = 0;
+    loop {
+        let filtered_lines = vector_function(lines, index, "co2");
+        if filtered_lines.len() <= 1 {
+            println!("CO2 rate: {}", filtered_lines[0]);
+            let co2_rate = usize::from_str_radix(filtered_lines[0], 2).unwrap();
+            println!("{}", co2_rate);
             break
         }
         index += 1;
@@ -20,7 +37,8 @@ fn main() {
     }
 }
 
-fn vector_function(mut param: Vec<&str>, index: usize) -> Vec<&str> {
+
+fn vector_function<'a>(mut param: Vec<&'a str>, index: usize, rate: &'a str) -> Vec<&'a str> {
     param.sort();
     let mut cutoff_point: usize = 0;
     for vector_element in &param {
@@ -33,11 +51,23 @@ fn vector_function(mut param: Vec<&str>, index: usize) -> Vec<&str> {
     let ones = param[cutoff_point..].to_vec();
 
     if zeroes.len() == ones.len() {
-        ones
+        if rate.eq("oxygen") {
+            return ones;
+        } else {
+            return zeroes;
+        }
     } else if zeroes.len() > ones.len() {
-        zeroes
+        if rate.eq("oxygen") {
+            return zeroes;
+        } else {
+            return ones;
+        }
     }
     else {
-        ones
+        if rate.eq("oxygen") {
+            return ones;
+        } else {
+            return zeroes;
+        }
     }
 }
