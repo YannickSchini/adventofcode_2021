054fe86077516d36b6c1fc28a9343b2be7c5d39d
Yannick Schini <yannick.schini@gmail.com>
2022-02-06 22:08:35 +0100

    Day04: Only missing a method to check if the grid is winning

--- day04/src/part1.rs
+++ day04/src/part1.rs
@@ -4,7 +4,6 @@ use std::str;
 use std::collections::HashMap;
 
 #[derive(Debug)]
-#[allow(dead_code)]
 enum BoxStatus {
     Checked,
     Unchecked,
@@ -14,16 +13,28 @@ const GRID_LENGTH: usize = 5;
 const GRID_WIDTH: usize = 5;
 
 #[derive(Debug)]
-#[allow(dead_code)]
 struct GridElement {
     status: BoxStatus,
     value: usize
 }
 
+impl GridElement {
+    fn check_grid_element(&mut self) {
+        self.status = BoxStatus::Checked
+    }
+}
+
 #[derive(Debug)]
-#[allow(dead_code)]
 struct BingoGrid {
     grid: HashMap<(usize, usize), GridElement>
+}
+
+impl BingoGrid {
+    fn check_a_number(&mut self, picked_number: usize) {
+        // let key = &self.grid.iter().find_map(|(key, val)| if val.value == picked_number { Some(key) } else { None });
+        let key = find_key_for_value(&self.grid, picked_number);
+        self.grid.get_mut(&key.unwrap()).unwrap().check_grid_element()
+    }
 }
 
 fn create_bingo_grid(lines: Vec<&str>) -> BingoGrid {
@@ -37,18 +48,15 @@ fn create_bingo_grid(lines: Vec<&str>) -> BingoGrid {
         }
         line_num += 1
     }
-    let mut bingo_grid = BingoGrid {
+    let bingo_grid = BingoGrid {
         grid: hashmap_under_construction,
     };
     bingo_grid
 }
 
-#[allow(dead_code)]
-impl BingoGrid {
-    fn check_a_number(&mut self, picked_number: usize) {
-        let key = &self.grid.iter().find_map(|(key, val)| if val.value == picked_number { Some(key) } else { None });
-        &mut self.grid.get_mut(key.unwrap()).unwrap().status = BoxStatus::Checked;
-    }
+fn find_key_for_value<'a>(map: &'a HashMap<(usize, usize), GridElement>, value: usize) -> Option<(usize, usize)> {
+    map.iter()
+        .find_map(|(key, val)| if val.value == value { Some(key.clone()) } else { None })
 }
 
 fn main() {
@@ -56,15 +64,23 @@ fn main() {
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let lines: Vec<&str> = contents.lines().collect();
-
     let picked_values: Vec<usize> = lines[0].split(",").map(|x| str::parse::<usize>(x).unwrap()).collect();
     println!("{:?}", picked_values);
-    let bingo_grid_1 = create_bingo_grid(lines[2..7].to_vec());
-    let bingo_grid_2 = create_bingo_grid(lines[8..13].to_vec());
-    let bingo_grid_3 = create_bingo_grid(lines[14..19].to_vec());
 
-    println!("{:?}", bingo_grid_1);
-    println!("{:?}", bingo_grid_2);
-    println!("{:?}", bingo_grid_3);
-
+    let mut bingo_grids: Vec<BingoGrid> = Vec::new();
+    let mut iterator = 0;
+    loop {
+        bingo_grids.push(create_bingo_grid(lines[2+iterator+(GRID_LENGTH*iterator)..2+iterator+(GRID_LENGTH*(iterator+1))].to_vec()));
+        iterator += 1;
+        if 2+iterator+(GRID_LENGTH*iterator) >= lines.len() {
+            break
+        }
+    }
+    for bingo in &bingo_grids {
+        println!("{:?}", bingo);
+    }
+    let mut test_bingo_grid = create_bingo_grid(lines[2..7].to_vec());
+    println!("{:?}",  test_bingo_grid);
+    test_bingo_grid.check_a_number(22);
+    println!("{:?}",  test_bingo_grid);
 }
