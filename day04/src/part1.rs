a568c25b6f9eea5a007a876504baac7fe1df8ee4
Yannick Schini <yannick.schini@qonto.com>
2022-02-08 20:54:49 +0100

    feat(day4): OK for test file, input file fails

--- day04/src/part1.rs
+++ day04/src/part1.rs
@@ -20,7 +20,7 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let lines: Vec<&str> = contents.lines().collect();
     let picked_values: Vec<usize> = lines[0].split(",").map(|x| str::parse::<usize>(x).unwrap()).collect();
-    println!("{:?}", picked_values);
+    // println!("{:?}", picked_values);
 
     let mut bingo_grids: Vec<bingo::BingoGrid> = Vec::new();
     let mut iterator = 0;
@@ -31,24 +31,35 @@ fn main() {
             break
         }
     }
-    for bingo in &bingo_grids {
-        println!("{:?}", bingo);
+
+    // for bingo in &bingo_grids {
+    //     println!("{:?}", bingo);
+    // }
+
+    'values: for value in picked_values {
+        for bingo_grid in bingo_grids.iter_mut() {
+            bingo_grid.check_a_number(value);
+            if bingo_grid.has_grid_won() {
+                println!("{}", bingo_grid.calculate_sum_of_unmarked()*value);
+                break 'values
+            }
+        }
     }
-    let mut test_bingo_grid = bingo::create_bingo_grid(lines[14..19].to_vec());
-    println!("{:?}",  test_bingo_grid);
-    println!("{}", test_bingo_grid.has_grid_won());
-    test_bingo_grid.check_a_number(7);
-    test_bingo_grid.check_a_number(4);
-    test_bingo_grid.check_a_number(9);
-    test_bingo_grid.check_a_number(5);
-    test_bingo_grid.check_a_number(11);
-    test_bingo_grid.check_a_number(17);
-    test_bingo_grid.check_a_number(23);
-    test_bingo_grid.check_a_number(2);
-    test_bingo_grid.check_a_number(0);
-    test_bingo_grid.check_a_number(14);
-    test_bingo_grid.check_a_number(21);
-    test_bingo_grid.check_a_number(24);
-    println!("{}", test_bingo_grid.has_grid_won());
-    println!("{}", test_bingo_grid.calculate_sum_of_unmarked());
+    // let mut test_bingo_grid = bingo::create_bingo_grid(lines[14..19].to_vec());
+    // println!("{:?}",  test_bingo_grid);
+    // println!("{}", test_bingo_grid.has_grid_won());
+    // test_bingo_grid.check_a_number(7);
+    // test_bingo_grid.check_a_number(4);
+    // test_bingo_grid.check_a_number(9);
+    // test_bingo_grid.check_a_number(5);
+    // test_bingo_grid.check_a_number(11);
+    // test_bingo_grid.check_a_number(17);
+    // test_bingo_grid.check_a_number(23);
+    // test_bingo_grid.check_a_number(2);
+    // test_bingo_grid.check_a_number(0);
+    // test_bingo_grid.check_a_number(14);
+    // test_bingo_grid.check_a_number(21);
+    // test_bingo_grid.check_a_number(24);
+    // println!("{}", test_bingo_grid.has_grid_won());
+    // println!("{}", test_bingo_grid.calculate_sum_of_unmarked());
 }
